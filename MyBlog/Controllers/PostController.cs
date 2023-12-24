33413a5b11fdf3af9726cc58e58ff57269730382
aysmdb <aysmdb@gmail.com>
2023-12-24 17:51:45 +0700

    pertemuan

--- MyBlog/Controllers/PostController.cs
+++ MyBlog/Controllers/PostController.cs
@@ -1,18 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
 
 namespace MyBlog.Controllers;
 
 public class PostController : Controller
 {
+    private readonly List<Post> _listPosts;
+
+    public PostController(){
+        _listPosts = GeneratePost();
+    }
+
     // GET
-    public IActionResult Index()
+    public IActionResult Index(int page = 1)
     {
-        return View();
+        ViewBag.NextPage = page + 1;
+        
+        int dataPerPage = 10;
+        int skip = dataPerPage * page - dataPerPage;
+
+        List<Post> data = _listPosts;
+        List<Post> filteredData = data
+            // .Where(post => post.Id <= 10)
+            .Skip(skip)
+            .Take(dataPerPage)
+            .OrderBy(post => post.Id) //Urutkan dari terkecil
+            // .OrderByDescending(post => post.Id) //Urutkan dari terbesar
+            .ToList();
+
+        return View(filteredData);
     }
     
-    public IActionResult Detail(string title)
+    public IActionResult Detail(int id)
     {
-        ViewBag.Title = title;
-        return View();
+        Post data = _listPosts.Where(post => post.Id == id)
+                .FirstOrDefault();
+
+        return View(data);
+    }
+
+    private List<Post> GeneratePost(){
+        List<Post> posts = new List<Post>();
+        int id = 1;
+        for(int i = 0;i < 100; i++ ){
+            posts.Add(
+                new Post(){
+                    Id = id,
+                    Title = "Judul " + id,
+                    Content = "Ini isi artikel",
+                    CreatedDate = DateTime.Now
+                }
+            );
+
+            id++;
+        }
+
+        return posts;
     }
 }
---
+++ MyBlog/Models/Post.cs
@@ -0,0 +1,8 @@
+namespace MyBlog.Models;
+
+public class Post {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
