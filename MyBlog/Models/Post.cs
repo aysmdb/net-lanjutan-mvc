19237c3611b73a18fbc97b13cc4338484eb7d2c6
aysmdb <aysmdb@gmail.com>
2023-12-24 20:08:13 +0700

    pertemuan 2

--- MyBlog/Models/Post.cs
+++ MyBlog/Models/Post.cs
@@ -4,5 +4,6 @@ public class Post {
     public int Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
+    public int Likes { get; set; }
     public DateTime CreatedDate { get; set; }
 }
